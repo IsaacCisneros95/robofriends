874d5ac2d3e5eb40191c71cf70df5d640916cfb1
Isaac Cisneros <isaac.cisneros@itjuana.com>
2022-09-17 16:30:30 -0700

    Updated packages

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,14 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 const Scroll = (props) => {
-	return (
-		<div style={{
-		overflowY: 'scroll', 
-		border: '2px solid gray', 
-		height: '550px'}}>
-			{props.children}
-		</div>
-	)
-}
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        border: "2px solid gray",
+        height: "640px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
 
 export default Scroll;
